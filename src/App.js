72634cc0ee6ecd43a1c069235a0ec54f41dccb58
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-11-25 12:39:30 +0500

    👌 IMPROVE: Transfer app to react.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Todo } from './todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todoList, setTodoList] = useState([]);
+	const [todoText, setTodoText] = useState('');
+
+	const addTodo = (event) => {
+		event.preventDefault();
+
+		if (todoText.length === 0) {
+			alert('Unable to add empty todo');
+		} else {
+			const tempList = [...todoList];
+			tempList.push({ text: todoText, checked: false });
+			setTodoList(tempList);
+			setTodoText('');
+		}
+	};
+	const handleInput = (event) => {
+		setTodoText(event.target.value);
+	};
+	const toggleTodo = (index, event) => {
+		const tempList = [...todoList];
+		tempList[index].checked = event.target.checked;
+		setTodoList(tempList);
+	};
+
+	const deleteTodo = (index) => {
+		const tempList = [...todoList];
+		tempList.splice(index, 1);
+		setTodoList(tempList);
+	};
+
+	console.log(todoList);
+	return (
+		<div className='App'>
+			<form onSubmit={addTodo} id='todo-body'>
+				<h1 id='main-heading'>Todofy</h1>
+				<div id='input-block'>
+					<input
+						type='text'
+						name='todo-input'
+						id='todo-input'
+						placeholder='What will you do today?'
+						onChange={handleInput}
+						value={todoText}
+					/>
+					<input type='submit' id='add-todo-btn' value='ADD' />
+				</div>
+				<div id='todos'>
+					{todoList.map((todo, i) => (
+						<Todo
+							todo={todo}
+							key={i}
+							index={i}
+							toggleTodo={toggleTodo}
+							deleteTodo={deleteTodo}
+						/>
+					))}
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/todo.js
@@ -0,0 +1,24 @@
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+export const Todo = ({ todo, index, toggleTodo, deleteTodo }) => {
+	console.log(index);
+	return (
+		<div className='todo'>
+			<input
+				type='checkbox'
+				name='todo'
+				checked={todo.checked}
+				onChange={(e) => toggleTodo(index, e)}
+			/>
+			<h4
+				className={`todo-text ${todo.checked ? 'line-through' : ''}`}
+			>
+				{todo.text}
+			</h4>
+			<div className='delete-icon' onClick={() => deleteTodo(index)}>
+				<FontAwesomeIcon icon={faTrash} />
+			</div>
+		</div>
+	);
+};
